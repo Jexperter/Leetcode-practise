41ca92557817c43813010abb465d3f084782523b
Jesan <akterjesan@gmail.com>
2024-10-03 19:34:15 +0100

    update 2

--- anagram.py
+++ anagram.py
@@ -7,9 +7,10 @@ for i in t:
     if i in s:
         seen.remove(i)
         print(seen)
-        if not seen:
-            print('true')
-    else:
-        print('false')
+
+if not seen:
+    print('true')
+else:
+    print('false')
     
     
